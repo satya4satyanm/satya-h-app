779c43022e0e8adeb1d4b1fbc292f840d57a69a3
umishs4 <satyanarayan.mishra@pearson.com>
2022-07-05 05:32:44 +0530

    initial commit

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -2,6 +2,7 @@ package com.example.heroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class HerokuApplication {
 		SpringApplication.run(HerokuApplication.class, args);
 	}
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 	return String.format("Hello %s!", name);
